8d1468c3867c799487bcefd8c2ae674f4b13dc1c
Rahul Patil <118384820+rahulptl556@users.noreply.github.com>
2023-01-02 12:10:40 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (89.31%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count=0;
+        if(word.size() == 1)
+            return 1;
+        
+        for(int i=0; i<word.size(); i++)
+            if(isupper(word[i]))
+                count++;
+        
+        if(count==1 && isupper(word[0]))
+            return 1;
+        if(count==0 || count==word.size())
+            return 1;
+        else
+            return 0;
+    }
+};
