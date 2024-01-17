ffad71022d4e53419d2b12c09f5a930875db622e
Rahul Patil <118384820+rahulptl556@users.noreply.github.com>
2024-01-17 21:36:27 +0530

    Time: 3 ms (69.43%), Space: 9.1 MB (75.01%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -7,9 +7,6 @@ public:
         while(s<e){
             int mid = s +(e-s)/2;
 
-            // if(arr[mid] > arr[mid+1] && arr[mid] > arr[mid-1]){
-            //     return mid;
-            // }
             if(arr[mid] > arr[mid+1]){
                 e = mid ;
             }
