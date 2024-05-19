1d8d56d9402ffb4372a751decea8d6ab2f81bf27
Rahul Patil <118384820+rahulptl556@users.noreply.github.com>
2024-05-19 12:50:19 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (21.48%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-  void rotate(vector<vector<int>> &matrix) {
-
-    int row = matrix.size();
-    for (int i = 0; i < row; i++) {
-
-      for (int j = i + 1; j < row; j++) {
-        swap(matrix[i][j], matrix[j][i]);
-      }
+    void rotate(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+        
+       // Transpose first
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        //Reverse each row
+        for(int i=0; i<n; i++){
+            int s = 0;
+            int e = n-1;
+            while(s<=e){
+                swap(matrix[i][s], matrix[i][e]);
+                s++;
+                e--;
+            }
+        }
+        
     }
-  
-       for (int i = 0; i < row; i++) {
-        reverse(matrix[i].begin(), matrix[i].end());
-    } 
-  }
 };
-
