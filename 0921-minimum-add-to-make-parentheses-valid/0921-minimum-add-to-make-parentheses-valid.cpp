437f6109a316a4c4724d342a79b9be84aac3a830
Rahul Patil <118384820+rahulptl556@users.noreply.github.com>
2024-05-24 09:37:59 +0530

    Time: 3 ms (44.00%), Space: 7.4 MB (68.14%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -4,28 +4,21 @@ public:
         int n = s.size();
         int left = 0;
         int count = 0;
-        stack <char> st;
         
         for(int i=0; i<n; i++){
             if(s[i] == '('){
-               st.push(s[i]);
+               left++;
             }
             else{
-                if(st.empty()){
+                if(left == 0){
                     count++;
                 }
                 else{
-                    st.pop();
+                    left--;;
                 }
             }
         }
-        if(st.empty()){
-            return count;
-        }
-        else{
-            count += st.size();
-            return count;
-        }
+        return left+count;
         
     }
 };
