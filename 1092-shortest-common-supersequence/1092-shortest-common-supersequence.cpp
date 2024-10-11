9a59978fe35a7318ee58155a7cc40c61a55f24e6
Rahul Patil <118384820+rahulptl556@users.noreply.github.com>
2024-10-11 11:36:36 -0400

    Time: 36 ms (6.71%), Space: 26 MB (15.63%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,57 +1,63 @@
 class Solution {
 public:
     
-    string shortestCommonSupersequence(string str1, string str2) {
+    int lcs(string &str1, string &str2, int n, int m, vector<vector<int>> &dp){
+        if(n == 0 || m == 0){
+            return dp[n][m] = 0;
+        }
+        
+        if(dp[n][m] != -1){
+            return  dp[n][m];
+        }
+        
+        if(str1[n-1] == str2[m-1]){
+            return  dp[n][m] = 1 + lcs(str1, str2, n-1, m-1, dp);
+        }
+        
+        else{
+            return  dp[n][m] = max(lcs(str1, str2, n, m-1, dp), lcs(str1, str2, n-1, m, dp));
+        }
+    }
+    
+    string shortestCommonSupersequence(string &str1, string &str2) {
         string s;
 
         int n = str1.length();
         int m = str2.length();
         
-        // Create the DP table
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
 
-        // Fill the DP table using the iterative LCS approach
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (str1[i-1] == str2[j-1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                } else {
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
-        }
+        lcs(str1, str2, n, m, dp); 
         
-        // Now, backtrack to construct the SCS
-        int i = n, j = m;
+        int i = n;
+        int j = m;
         
-        while (i != 0 && j != 0) {
-            if (str1[i-1] == str2[j-1]) {
+        // Creating the string  
+        while(i!=0 && j!=0){
+            if(str1[i-1] == str2[j-1]){
                 s.push_back(str1[i-1]);
                 i--;
                 j--;
             }
-            else if (dp[i-1][j] > dp[i][j-1]) {
+            else if(dp[i-1][j] > dp[i][j-1]){
                 s.push_back(str1[i-1]);
                 i--;
-            } else {
+            }
+            else{
                 s.push_back(str2[j-1]);
                 j--;
             }
         }
         
-        // Add remaining characters of str1 (if any)
-        while (i != 0) {
+        while(i != 0){
             s.push_back(str1[i-1]);
             i--;
         }
-
-        // Add remaining characters of str2 (if any)
-        while (j != 0) {
+        while(j != 0){
             s.push_back(str2[j-1]);
             j--;
         }
         
-        // Reverse the result since it was constructed backwards
         reverse(s.begin(), s.end());
         return s;
     }
